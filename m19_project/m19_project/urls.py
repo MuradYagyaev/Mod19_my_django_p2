06e10a4306d58a4f559cdbf71e8eb71b5302fd38
murad <moro11@mail.ru>
2024-10-10 23:43:50 +0300

    10.10.2024 - Урок 3

--- m19_project/m19_project/urls.py
+++ m19_project/m19_project/urls.py
@@ -17,6 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from task1.views import Platform, Games, Cart
+from task1.views import sign_up_by_django, sign_up_by_html
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('platform/', Platform.as_view()),
+    path('platform/games/', Games.as_view()),
+    path('platform/cart/', Cart.as_view()),
+    path('django_sign_up/', sign_up_by_django),
+    path('html_sign_up/', sign_up_by_html),
 ]
--- m19_project/task1/views.py
+++ m19_project/task1/views.py
@@ -1,3 +1,98 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+
+from task1.forms import UserRegister
+
+users = ['user1', 'user2', 'user3', 'user4', 'user5']
+
 
 # Create your views here.
+class Platform(TemplateView):
+    template_name = 'platform.html'
+    title = 'Главная'
+    pagename = 'Главная страница'
+    content = 'Мы рады приветствовать Вас в нашем магазине компьютерных игр!'
+    extra_context = {
+        'title': title,
+        'pagename': pagename,
+        'content': content,
+    }
+
+
+class Games(TemplateView):
+    template_name = 'games.html'
+    title = 'Каталог'
+    pagename = 'Игры'
+    games = ['Atomic Heart', 'Cyberpunk 2077', 'PayDay 2', 'Doom 2']
+    extra_context = {
+        'title': title,
+        'pagename': pagename,
+        'games': games,
+    }
+
+
+class Cart(TemplateView):
+    template_name = 'cart.html'
+    title = 'Корзина'
+    pagename = 'Корзина'
+    content = 'Извините, ваша корзина пуста'
+    extra_context = {
+        'title': title,
+        'pagename': pagename,
+        'content': content,
+    }
+
+
+def check_data(data):
+    if data['username'] in users:
+        return 'Пользователь уже существует'
+    elif data['password'] != data['repeat_password']:
+        return 'Пароли не совпадают'
+    elif int(data['age']) < 18:
+        return 'Вы должны быть старше 18'
+    return f'Приветствуем, {data['username']}!'
+
+def sign_up_by_django(request):
+    info = {}
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = form.cleaned_data['age']
+            info = {'username': username,
+                    'password': password,
+                    'repeat_password': repeat_password,
+                    'age': age,
+                    }
+            check = check_data(info)
+            if 'Приветствуем' in check:
+                info.update({'greetings': check})
+            else:
+                info.update({'error': check})
+    else:
+        form = UserRegister()
+    info.update({'form': form})
+    return render(request, 'registration_page.html', info)
+
+
+def sign_up_by_html(request):
+    info = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        repeat_password = request.POST.get('repeat_password')
+        age = request.POST.get('age')
+        info = {'username': username,
+                'password': password,
+                'repeat_password': repeat_password,
+                'age': age,
+                }
+        check = check_data(info)
+        if 'Приветствуем' in check:
+            info.update({'greetings': check})
+        else:
+            info.update({'error': check})
+    return render(request, 'registration_page.html', info)
